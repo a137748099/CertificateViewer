6e676ebd4ecbd9d7fd56707bba79ae8cf04c0fa4
rd13 <rd13@wotrus.com>
2021-09-27 09:20:34 +0800

    删除测试代码

--- CertViewer/main.cpp
+++ CertViewer/main.cpp
@@ -8,25 +8,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTranslator translator;
-    // look up e.g. :/translations/myapp_de.qm
     if (translator.load(":/linguist/linguist_cn.qm"))
         a.installTranslator(&translator);
-
-    enum string{
-        x1,
-        x2,
-        x3=10,
-        x4,
-        x5,
-    } x;
-
-    qDebug() << __FUNCTION__ << "song" <<x;
-
-    unsigned char *p1;
-    unsigned long *p2;
-    p1=(unsigned char *)0x801000;
-    p2=(unsigned long *)0x810000;
-    qDebug() << __FUNCTION__ << "song" << p1 + 5 << p2 + 5;
 
     MainWidget w;
     w.show();
